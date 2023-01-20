acf5adf2bf48802f70118bda261c29f397734a9f
andrei-stoean <andrei.stoean@yahoo.com>
2023-01-20 23:31:48 +0200

    Created Testing Framework

--- app/src/androidTest/java/com/example/firstandroidapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/firstandroidapp/ExampleInstrumentedTest.java
@@ -1,13 +1,24 @@
 package com.example.firstandroidapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +28,45 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.firstandroidapp", appContext.getPackageName());
+    private static final String PACKAGE_NAME
+            = "com.example.firstandroidapp";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice device;
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        device.pressHome();
+
+        Context context = ApplicationProvider.getApplicationContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(PACKAGE_NAME);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME).depth(0)),
+                LAUNCH_TIMEOUT);
     }
-}
+
+    @Test
+    public void checkErrorMessageForEmptyField() throws UiObjectNotFoundException {
+        pressLoginButton();
+
+        UiObject textInput = device.findObject(new UiSelector()
+                .className("android.widget.EditText")
+                .resourceId(String.valueOf(R.id.name)));
+
+
+    }
+
+    public void pressLoginButton() throws UiObjectNotFoundException {
+        UiObject loginButton = device.findObject(new UiSelector()
+                .text("LOGIN")
+                .className("android.widget.Button"));
+
+        if(loginButton.exists() && loginButton.isEnabled()) {
+            loginButton.click();
+        }
+    }
+
+    }
